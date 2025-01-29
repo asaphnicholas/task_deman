6f0bd0f2fa2152c3480779ec6056f20a46f954aa
asaphnicholas <asaphnicholas@gmail.com>
2025-01-29 14:32:02 -0300

    Projeto completo

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,54 @@
+dmx.config({
+  "painel_aluno": {
+    "repeat_tasks_alunos": {
+      "meta": [
+        {
+          "name": "id",
+          "type": "number"
+        },
+        {
+          "name": "nome",
+          "type": "text"
+        },
+        {
+          "name": "descricao",
+          "type": "text"
+        },
+        {
+          "name": "estimativa_horas",
+          "type": "number"
+        },
+        {
+          "name": "nivel",
+          "type": "text"
+        }
+      ],
+      "outputType": "text"
+    },
+    "data_detalhes_tarefa": {
+      "meta": [
+        {
+          "name": "id",
+          "type": "number"
+        },
+        {
+          "name": "nome",
+          "type": "text"
+        },
+        {
+          "name": "descricao",
+          "type": "text"
+        },
+        {
+          "name": "estimativa_horas",
+          "type": "number"
+        },
+        {
+          "name": "nivel",
+          "type": "text"
+        }
+      ],
+      "outputType": "text"
+    }
+  }
+});
